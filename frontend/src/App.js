bce1aac51bd21d2d372ecbc92dce4ee142badb62
Chetana21 <pchetana691@gmail.com>
2024-03-07 17:07:36 +0530

    setting up frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import { Button } from '@chakra-ui/react'
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      Bidding made Easy !!
+      
+      <Button colorScheme='blue'>Button</Button>
+    </div>
+  );
+}
+
+export default App;
