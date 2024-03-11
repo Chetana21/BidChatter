30a59258937277b57e99b48fb7003918c523aa4a
Chetana21 <pchetana691@gmail.com>
2024-03-11 23:39:34 +0530

    Connect app to mongodb

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+//Create a function ot connect ot our database
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDb Connected on ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.log(`Error: ${error.message}`.red.bold);
+    process.exit();
+  }
+};
+
+module.exports=connectDB;
